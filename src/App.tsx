101b71d201a6b7f63358cc22247066c9c417cfe2
Yadong Xie <vthinkxie@gmail.com>
2023-06-10 18:39:06 +0800

    feat: add speach rec

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import {
   AlipayOutlined,
+  AudioOutlined,
   ShoppingCartOutlined,
   TaobaoOutlined,
 } from "@ant-design/icons";
 import { ChatModule } from "@mlc-ai/web-llm";
-import { Button, Divider, Input, Spin } from "antd";
-import { ReactNode, useEffect, useState } from "react";
+import { Divider, Input, Spin } from "antd";
+import { ReactNode, useEffect, useRef, useState } from "react";
 
 interface ComponentItem {
   name: string;
@@ -49,45 +50,94 @@ const useVicunaChat = () => {
   return chat;
 };
 
+const useWebSpeechRecognition = (onFinish: () => void) => {
+  const recognition = useRef(new (window as any).webkitSpeechRecognition());
+  const ignoreOnend = useRef(false);
+  const [recognizing, setRecognizing] = useState(false);
+  const [transcript, setTranscript] = useState("");
+  recognition.current.continuous = true;
+  recognition.current.interimResults = true;
+  recognition.current.onerror = () => {
+    ignoreOnend.current = true;
+  };
+  recognition.current.onstart = () => {
+    setTranscript("");
+    setRecognizing(true);
+  };
+  recognition.current.onend = function () {
+    setRecognizing(false);
+    if (ignoreOnend.current) {
+      return;
+    }
+    onFinish();
+  };
+
+  recognition.current.onresult = (event: any) => {
+    if (typeof event.results == "undefined") {
+      recognition.current.onend = null;
+      recognition.current.stop();
+      return;
+    }
+    for (let i = event.resultIndex; i < event.results.length; ++i) {
+      if (event.results[i].isFinal) {
+        setTranscript(transcript + event.results[i][0].transcript);
+        recognition.current.stop();
+      }
+    }
+  };
+
+  return [recognition.current, recognizing, transcript];
+};
+
 function App() {
   const chat = useVicunaChat();
+  const [recognition, recognizing, transcript] = useWebSpeechRecognition(() => {
+    setLoading(true);
+    chat!
+      .generate(transcript, () => setLoading(false))
+      .then((res) => {
+        const regex = /```json\n([\s\S]*?)```/gm;
+        const match = regex.exec(res);
+        const json = match ? match[1] : null;
+        console.log(res, json);
+        try {
+          setComponent(JSON.parse(json as string));
+        } catch (e) {
+          console.log(e);
+        }
+      });
+  });
   const [loading, setLoading] = useState(false);
   const [component, setComponent] = useState<ComponentItem | null>(null);
-  const [text, setText] = useState("");
+  const suffix = (
+    <AudioOutlined
+      onClick={() => {
+        if (recognizing) {
+          recognition.stop();
+        } else {
+          recognition.start();
+        }
+      }}
+      style={{
+        fontSize: 16,
+        color: "#1677ff",
+      }}
+    />
+  );
   return (
     <Spin spinning={!chat}>
-      <Input.TextArea
-        value={text}
-        onChange={(v) => setText(v.target.value)}
-      ></Input.TextArea>
+      <Input
+        placeholder="input search text"
+        size="large"
+        suffix={suffix}
+        value={transcript}
+      />
       <Divider></Divider>
-      <Button
-        type="primary"
-        block
-        loading={loading}
-        onClick={() => {
-          setLoading(true);
-          chat!
-            .generate(text, () => setLoading(false))
-            .then((res) => {
-              const regex = /```json\n([\s\S]*?)```/gm;
-              const match = regex.exec(res);
-              const json = match ? match[1] : null;
-              console.log(res, json);
-              try {
-                setComponent(JSON.parse(json as string));
-              } catch (e) {
-                console.log(e);
-              }
-            });
-        }}
-      >
-        Submit
-      </Button>
-      <Divider></Divider>
-      <div style={{ fontSize: "128px", textAlign: "center" }}>
-        {component && componentMap[component.name]}
-      </div>
+      <Spin spinning={loading}>
+        <div style={{ fontSize: "128px", textAlign: "center" }}>
+          {component && componentMap[component.name]}
+        </div>
+      </Spin>
     </Spin>
   );
 }
